b2552f9b7a84763716a21686ea44ac57bed80156
VoMinhQuan184615 <vominhqun14112004@gmail.com>
2025-11-08 02:10:18 +0700

    feat: Implement CRUD task

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,14 +1,52 @@
-export const getAllTasks = (req, res) => {
-    res.status(200).json({ message: 'List of tasks' });
+import Task from "../models/Task.js"; 
+
+
+
+export const getAllTasks = async (req, res) => {
+   try {
+    const task = await Task.find().sort({ createdAt: -1 });
+    res.status(200).json(task);
+   }
+    catch (error) {
+        console.error(error);
+    res.status(500).json({ message: error.message });
+   }
 };
 
-export const createTask = (req, res) => {
-    res.status(201).json({ message: 'Task created' });
+export const createTask = async(req, res) => {
+    try {
+        const {title} = req.body;
+        const newTask = new Task({title});
+        const savedTask = await newTask.save();
+        res.status(201).json(savedTask);
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
 };
 
-export const updateTask = (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} updated` });
+export const updateTask = async (req, res) => {
+    try {
+        const { title, status, completeAt } = req.body;
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, { title, status, completeAt }, { new: true });
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json(updatedTask);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
 };
-export const deleteTask = (req, res) => {
-    res.status(200).json({ message: `Task ${req.params.id} deleted` });
+export const deleteTask = async (req, res) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json({ message: `Task ${req.params.id} deleted` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
 };
--- src/server.js
+++ src/server.js
@@ -5,13 +5,21 @@ import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-connectDB();
+
+
+app.use(express.json());
 
 app.use("/api/tasks",tasksRouter)
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Database connection failed:", error);
+    process.exit(1);
 });
 
